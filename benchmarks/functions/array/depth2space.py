8a2a62c1a21bdc3c1acade82ca09c28fe59c4858
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-03-23 19:00:07 +0900

    add benchmark for space2depth and depth2space

---
+++ benchmarks/functions/array/depth2space.py
@@ -0,0 +1,22 @@
+import chainer.functions as F
+
+from benchmarks.functions import FunctionBenchmark
+from benchmarks.utils import backends
+
+
+@backends('gpu', 'cpu')
+class Depth2Space(FunctionBenchmark):
+
+    def setup(self):
+        xp = self.xp
+        x = xp.random.randn(128, 80, 30, 20).astype(xp.float32)
+        gy = xp.random.randn(128, 20, 60, 40).astype(xp.float32)
+        r = 2
+        func = lambda x: F.depth2space(x, r)
+        self.setup_benchmark(func, (x,), (gy,))
+
+    def time_forward(self):
+        self.forward()
+
+    def time_backward(self):
+        self.backward()
---
+++ benchmarks/functions/array/space2depth.py
@@ -0,0 +1,22 @@
+import chainer.functions as F
+
+from benchmarks.functions import FunctionBenchmark
+from benchmarks.utils import backends
+
+
+@backends('gpu', 'cpu')
+class Space2Depth(FunctionBenchmark):
+
+    def setup(self):
+        xp = self.xp
+        x = xp.random.randn(128, 20, 60, 40).astype(xp.float32)
+        gy = xp.random.randn(128, 80, 30, 20).astype(xp.float32)
+        r = 2
+        func = lambda x: F.space2depth(x, r)
+        self.setup_benchmark(func, (x,), (gy,))
+
+    def time_forward(self):
+        self.forward()
+
+    def time_backward(self):
+        self.backward()
