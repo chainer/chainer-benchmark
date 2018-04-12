5a9ecc58386c6edcffccd14719697c735c7b472f
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-04-12 14:27:43 +0900

    change MNIST example benchmark to measure prediction performance

--- benchmarks/mnist/mnist.py
+++ benchmarks/mnist/mnist.py
@@ -28,38 +28,67 @@ class Network(chainer.Chain):
 
 class Application(object):
 
-    def main(self, units, epoch, batchsize):
-        model = L.Classifier(Network(units, 10))
+    def __init__(self, units):
+        self.model = L.Classifier(Network(units, 10))
 
-        gpu = -1
+        self.gpu = -1
         if is_backend_gpu():
-            gpu = 0
-            chainer.cuda.get_device_from_id(gpu).use()
-            model.to_gpu()
+            self.gpu = 0
+            chainer.cuda.get_device_from_id(self.gpu).use()
+            self.model.to_gpu()
         elif is_backend_ideep():
-            model.to_intel64()
+            self.model.to_intel64()
 
+    def train(self, train, test, epoch, batchsize):
         optimizer = chainer.optimizers.MomentumSGD()
-        optimizer.setup(model)
+        optimizer.setup(self.model)
 
-        train, test = chainer.datasets.get_mnist()
-        train_iter = chainer.iterators.SerialIterator(
-            train, batchsize)
+        train_iter = chainer.iterators.SerialIterator(train, batchsize)
         test_iter = chainer.iterators.SerialIterator(
             test, batchsize, repeat=False, shuffle=False)
 
         updater = training.updater.StandardUpdater(
-            train_iter, optimizer, device=gpu)
+            train_iter, optimizer, device=self.gpu)
         trainer = training.Trainer(updater, (epoch, 'epoch'))
-        trainer.extend(extensions.Evaluator(test_iter, model, device=gpu))
+        trainer.extend(extensions.Evaluator(
+            test_iter, self.model, device=self.gpu))
 
         trainer.run()
+
+    def predict(self, data):
+        with chainer.no_backprop_mode(), chainer.using_config('train', False):
+            self.model.predictor(data)
+
+
+class _MLPBase(BenchmarkBase):
+    def setup(self, units, batchsize):
+        self.app = Application(units=units)
+        self.train_data, self.test_data = chainer.datasets.get_mnist()
+
+        xp = self.xp
+        arrays = [xp.asarray(x[0]) for x in self.train_data]
+        self.predict_data = xp.concatenate(arrays).reshape(len(arrays), -1)
+
+        # Initialize.
+        self.app.predict(self.predict_data[0:1])
 
 
 @backends('gpu', 'gpu-cudnn', 'cpu', 'cpu-ideep')
 @parameterize([
     ('units', [10, 100, 150]),
+    ('batchsize', [100]),
 ])
-class MLP(BenchmarkBase):
-    def time_overall(self, units):
-        Application().main(units=units, epoch=1, batchsize=100)
+class MLPTrain(_MLPBase):
+    def time_train(self, units, batchsize):
+        self.app.train(
+            self.train_data, self.test_data, epoch=1, batchsize=batchsize)
+
+
+@backends('gpu', 'gpu-cudnn', 'cpu', 'cpu-ideep')
+@parameterize([
+    ('units', [10, 100, 150, 1000]),
+    ('batchsize', [1, 100]),
+])
+class MLPPredict(_MLPBase):
+    def time_predict(self, units, batchsize):
+        self.app.predict(chainer.Variable(self.predict_data[0:batchsize]))
